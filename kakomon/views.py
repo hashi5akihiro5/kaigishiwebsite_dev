8d9b1acda04e6468ca5754dac4b15cba5af4401a
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-07-12 12:14:46 +0900

    [add] index.html 表示

--- kakomon/views.py
+++ kakomon/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+""" Top画面 """
+
+
+class IndexView(TemplateView):
+    template_name = "kakomon/index.html"

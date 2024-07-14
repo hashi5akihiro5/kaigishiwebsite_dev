c3b665d46c146e9cc9a138e12228242f1fcb37d3
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-07-14 13:48:47 +0900

    [add] exam, constants, function 追加

--- kakomon/urls.py
+++ kakomon/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 app_name = "kakomon"
 
-urlpatterns = [path("", views.IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path(
+        "<exam_type>/<navigation_or_engineering>/<grade>/",
+        views.ExamListView.as_view(),
+        name="list_exam",
+    ),
+]

50fd9fbfeab2616b423c5b78fe2d1709dcb77842
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-10-03 08:44:18 +0900

    [update] 画像表示 設定

--- config/settings.py
+++ config/settings.py
@@ -110,6 +110,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = "static/"
 
 
+MEDIA_ROOT = BASE_DIR.joinpath("media")
+MEDIA_URL = "/media/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
